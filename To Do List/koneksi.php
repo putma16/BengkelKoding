e36f07b3b30666b7dbc7855ea44cdc5b8f980908
putma16 <54889900+putma16@users.noreply.github.com>
2023-10-19 11:31:57 +0700

    first commit

---
+++ To Do List/koneksi.php
@@ -0,0 +1,10 @@
+<?php 
+$databaseHost = 'localhost';
+$databaseName = 'kegiatan';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, 
+    $databasePassword, $databaseName);
+?>
+
